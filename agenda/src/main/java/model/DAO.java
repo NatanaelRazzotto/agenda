aedf5b8cec400eef4d55ec6f299463b0b489b884
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2023-02-16 19:55:06 -0300

    Action Post contacts

--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -2,15 +2,17 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DAO {
-	/**Modulo de conexão**/
-	//Parametros de conexão
+	/** Modulo de conexão **/
+	// Parametros de conexão
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
-	private String password = "1111"; //Mudar para senha real 
-	//Metodo de conexao
+	private String password = "*******"; // Mudar para senha real
+	// Metodo de conexao
+
 	private Connection conectar() {
 		Connection con = null;
 		try {
@@ -21,8 +23,31 @@ public class DAO {
 			System.out.println(e);
 			return null;
 		}
-	} 
-	//teste de conexão
+	}
+
+	/* CRUD CREATE */
+	public void inserirContato(JavaBeans contato) {
+		String create = "insert into contatos (nome,fone,email) values (?,?,?)";
+		try {
+			// abrir a conexao com o banco;
+			Connection con = conectar();
+			// Preparar a query para execução no banco de dados
+			PreparedStatement pst = con.prepareStatement(create);
+			// Substituir os parametros (?) pelo conteudo das variaiveis
+			pst.setString(1, contato.getNome());
+			pst.setString(2, contato.getFone());
+			pst.setString(3, contato.getEmail());
+			// Executar a query
+			pst.executeUpdate();
+			// encerar a conexao com o banco
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+
+	}
+
+	// teste de conexão
 	public void testeConexao() {
 		try {
 			Connection con = conectar();
