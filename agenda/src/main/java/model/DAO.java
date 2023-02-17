49d1a09b72d12b834b9533aad5a485fad6cf0a30
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2023-02-16 21:04:10 -0300

    List data Contatos

--- agenda/src/main/java/model/DAO.java
+++ agenda/src/main/java/model/DAO.java
@@ -3,6 +3,8 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class DAO {
 	/** Modulo de conexão **/
@@ -10,7 +12,7 @@ public class DAO {
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
 	private String user = "root";
-	private String password = "*******"; // Mudar para senha real
+	private String password = "*********"; // Mudar para senha real
 	// Metodo de conexao
 
 	private Connection conectar() {
@@ -45,6 +47,33 @@ public class DAO {
 			System.out.println(e);
 		}
 
+	}
+
+	/** CRUD READ **/
+	public ArrayList<JavaBeans> listarContatos() {
+		// criando um objeto para acessar a classe javabeans
+		ArrayList<JavaBeans> contatos = new ArrayList<>();
+		String read = "select * from contatos order by nome";
+		try {
+			Connection con = conectar();
+			PreparedStatement pst = con.prepareStatement(read);
+			ResultSet rs = pst.executeQuery();
+			// o laco abaixo sera executado enquanto ouver contatos
+			while (rs.next()) {
+				// variaveis de apoio que recebem os dados do banco
+				String idcon = rs.getString(1);
+				String nome = rs.getString(2);
+				String fone = rs.getString(3);
+				String email = rs.getString(4);
+				// populando o arraylist
+				contatos.add(new JavaBeans(idcon, nome, fone, email));
+			}
+			con.close();
+			return contatos;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
 	}
 
 	// teste de conexão
--- agenda/src/main/webapp/agenda.jsp
+++ agenda/src/main/webapp/agenda.jsp
@@ -1,5 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
     pageEncoding="utf-8"%>
+    <%@ page import="model.JavaBeans" %>
+     <%@ page import="java.util.ArrayList" %>
+     <%
+     ArrayList<JavaBeans> lista = (ArrayList<JavaBeans>) request.getAttribute("contatos");
+     
+ 	/*for (int i = 0; i < lista.size(); i++) {
+		out.println(lista.get(i).getIdcon());
+		out.println(lista.get(i).getNome());
+		out.println(lista.get(i).getFone());
+		out.println(lista.get(i).getEmail());
+	}*/
+     %>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -10,6 +22,26 @@
 </head>
 <body>
 	<h1>Agenda de Contatos</h1>	
+	<table id="tabela">
+		<thead>
+		<tr>
+		<th>Id</th>
+		<th>Nome</th>
+		<th>Fone</th>
+		<th>E-mail</th>
+		</tr>
+		</thead>
+		<tbody>
+		<%for (int i = 0; i < lista.size(); i++){%>
+			<tr>
+				<td><%=lista.get(i).getIdcon()%></td>
+				<td><%=lista.get(i).getNome() %>
+				<td><%=lista.get(i).getFone() %>
+				<td><%=lista.get(i).getEmail() %>				
+			</tr>
+		<%} %>
+		</tbody>
+	</table>
 	<a href="novo.html" class="Botao1">Novo Contato</a>
 </body>
 </html>
